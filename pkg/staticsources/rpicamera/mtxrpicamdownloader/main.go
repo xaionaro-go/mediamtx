79ecbc3e138f93d6a4758c3e3e3427159028596c
Dmitrii Okunev <xaionaro@dx.center>
2025-04-06 14:26:18 +0100

    Add missing mtxrpicam artifacts

--- pkg/staticsources/rpicamera/mtxrpicamdownloader/main.go
+++ pkg/staticsources/rpicamera/mtxrpicamdownloader/main.go
@@ -69,7 +69,7 @@ func do() error {
 			return err
 		}
 
-		res, err := http.Get("https://github.com/xaionaro-go/mediamtx-rpicamera/releases/download/" + version + "/" + f)
+		res, err := http.Get("https://github.com/bluenviron/mediamtx-rpicamera/releases/download/" + version + "/" + f)
 		if err != nil {
 			return err
 		}
